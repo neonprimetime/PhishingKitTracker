66016a06c0dec692d1b4afb35491832b3710c9ce
neonprimetime security (Justin C Miller) <neonprimetime.geo@yahoo.com>
2019-01-07 22:14:40 -0600

    fixed zips that are not zips
    fixed zips that are not zips

--- phishingKitParser.py
+++ phishingKitParser.py
@@ -104,7 +104,10 @@ if(proceed == 1):
   if(settings.debug):
    print("unzipping file '{0}' to '{1}'".format(filename,extractedfoldername))
   with zipfile.ZipFile(filename,'r') as zip_ref:
-   zip_ref.extractall(extractedfoldername)
+   try:
+    zip_ref.extractall(extractedfoldername)
+   except:
+    print("failed to unzip '{0}'".format(filename))
   if(settings.debug):
    print("file unzipped to '{0}'".format(extractedfoldername))
   if(settings.debug):
