da50e98cab124f19c2ce6b3ccbddea6ef6ee0f8b
neonprimetime security (Justin C Miller) <neonprimetime.geo@yahoo.com>
2019-05-06 14:42:54 -0500

    case insensitive
    case insensitive

--- ExtractFromTwitterPost.py
+++ ExtractFromTwitterPost.py
@@ -7,10 +7,10 @@ arguments.add_argument("-f", "--file", type=str, required=True, help="Path to fi
 arguments.add_argument("-c", "--comments", action="store_true", required=False, help="Output individual twitter replies too")
 settings = arguments.parse_args()
 
-emailregex = r'(\s|\"|\,|\,\s+)([^\@\s\"]+\@[^\s\"]+)(\[\.\].{2,3}|\.[^\]].{1,2})(\"|\s|\,)'
-domainregex = r'(hxxp|http|https|hxxps)\:\/\/([^\/\s]+)(\/|\s)'
-emaildomainregex = r'[\@]([^\.]+)\.'
-emailkeepregex = r'(gmail|yahoo|zoho|yandex|aol|mail\.ru|outlook|hotmail|protonmail|live\.com|mail\.com)'
+emailregex = r'(?i)(\s|\"|\,|\,\s+)([^\@\s\"]+\@[^\s\"]+)(\[\.\].{2,3}|\.[^\]].{1,2})(\"|\s|\,)'
+domainregex = r'(?i)(hxxp|http|https|hxxps)\:\/\/([^\/\s]+)(\/|\s)'
+emaildomainregex = r'(?i)[\@]([^\.]+)\.'
+emailkeepregex = r'(?i)(gmail|yahoo|zoho|yandex|aol|mail\.ru|outlook|hotmail|protonmail|live\.com|mail\.com)'
 
 splitchoice1 = "@neonprimetime"
 splitchoice2 = "Phisher"
